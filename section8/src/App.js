5fd664da402d4248efe6cf0b7ac79da0ccd5d572
ogawayouji <satsumaimo697@gmail.com>
2021-06-25 22:53:41 +0900

    section8

---
+++ section8/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./styles.css";
+import axios from 'axios';
+
+export default function App() {
+  const onClickUsers = () => {
+    // alert('users');
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res)=> {
+      console.log(res.data);
+    }).catch((err) => console.log(err));
+  };
+
+  const onClickUser1 = () => {
+    // alert('user1')
+    axios.get("https://jsonplaceholder.typicode.com/users/1").then((res)=> {
+      console.log(res.data);
+    }).catch((err) => console.log(err));
+  }
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  )
+}
+

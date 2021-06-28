2016708f7317cab46e822982b852f3e96752733c
ogawayouji <satsumaimo697@gmail.com>
2021-06-29 02:14:49 +0900

    section9

--- section9/src/App.tsx
+++ section9/src/App.tsx
@@ -5,7 +5,7 @@
 import React from "react";
 // import "./styles.css"
 // import { Practice1 } from "i"
-import { Practice1 } from "./practices/Practice1";
+import { Practice1 } from "./practices/Practice1/Practice1";
 import { Practice2 } from "./practices/Practice2";
 import { Practice3 } from "./practices/Practice3";
 import { Practice4 } from "./practices/Practice4";
